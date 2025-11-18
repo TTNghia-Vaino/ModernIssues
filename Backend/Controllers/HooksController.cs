f0da5cd5872a7a33ba30c14290471d6ee7630074
TTNghia-Vaino <ttnghia004@gmail.com>
2025-11-18 15:31:08 +0700

    change route api, change appsettings

--- Backend/Controllers/HooksController.cs
+++ Backend/Controllers/HooksController.cs
@@ -6,7 +6,7 @@ using ModernIssues.Models.Entities;
 namespace ModernIssues.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class HooksController : Controller
     {
         private readonly IHooksService _hooksService;
