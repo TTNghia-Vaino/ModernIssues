1741839c1b3c760f29d35c75c400a02cd9045ce1
Manh12347 <lemanh151148@gmail.com>
2025-11-24 22:16:18 +0700

    Remove all backend code, keep Backend directory only

--- Backend/Models/DTOs/ResetPasswordRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ModernIssues.Models.DTOs
-{
-    public class ResetPasswordRequest
-    {
-        public string TempToken { get; set; }
-        public string NewPassword { get; set; }
-    }
-}
