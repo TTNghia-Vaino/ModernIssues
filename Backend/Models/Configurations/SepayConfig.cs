f661d3898ec06b9c4abd6fe96d2c90cb988ec6e2
Manh12347 <lemanh151148@gmail.com>
2025-11-24 22:17:09 +0700

    Remove all backend code, keep Backend directory only

--- Backend/Models/Configurations/SepayConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ModernIssues.Models.Configurations
-{
-    public class SepayConfig
-    {
-        public string AccountNumber { get; set; }
-        public string BankName { get; set; }
-    }
-}
--- Backend/Models/DTOs/LoginRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ModernIssues.Models.DTOs
-{
-    public class LoginRequest
-    {
-        public string Email { get; set; } = null!;
-        public string Password { get; set; } = null!;
-    }
-}
