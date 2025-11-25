162311ce54d6148e3edf9a99aa220ced55e1fc23
TTNghia-Vaino <ttnghia004@gmail.com>
2025-11-26 03:03:42 +0700

    add path to start.sh, change source root

--- Python/app/start.sh
+++ Python/app/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-source /root/RAG/venv/bin/activate
+source /var/www/modernissues/python/venv/bin/activate
+cd /var/www/modernissues/python/app
 exec uvicorn main:app --host 0.0.0.0 --port 8000
