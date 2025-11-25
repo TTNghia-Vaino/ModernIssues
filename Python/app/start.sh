4dfb5d571cacf9bd414deadf94f193bc35fd13da
TTNghia-Vaino <ttnghia004@gmail.com>
2025-11-26 03:07:04 +0700

    change source, path

--- Python/app/start.sh
+++ Python/app/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /var/www/modernissues/python/venv/bin/activate
-cd /var/www/modernissues/python/app
+source /var/www/modernissues/Python/venv/bin/activate
+cd /var/www/modernissues/Python/app
 exec uvicorn main:app --host 0.0.0.0 --port 8000
