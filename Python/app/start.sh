fc2730921d5c2bd4363c0d6d7b78c8d9ddf6ce10
TTNghia-Vaino <ttnghia004@gmail.com>
2025-11-26 03:13:12 +0700

    delete path, call app

--- Python/app/start.sh
+++ Python/app/start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 source /var/www/modernissues/Python/venv/bin/activate
-cd /var/www/modernissues/Python/app
-exec uvicorn main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
